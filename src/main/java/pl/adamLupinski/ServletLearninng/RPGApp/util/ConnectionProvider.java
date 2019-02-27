c516943e4b29a29ce7cfacf9aa66e5a2c1bb0799
MagicznyKocik <azlupinski@gmail.com>
2019-02-27 11:47:31 +0100

    db connection configuration added

---
+++ src/main/java/pl/adamLupinski/ServletLearninng/RPGApp/util/ConnectionProvider.java
@@ -0,0 +1,31 @@
+package pl.adamLupinski.ServletLearninng.RPGApp.util;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionProvider {
+
+    private static DataSource dataSource;
+
+    public static Connection getConnection() throws SQLException {
+        return getDataSource().getConnection();
+    }
+
+    private static DataSource getDataSource(){
+        if (dataSource == null){
+            try {
+                Context initialContext = new InitialContext();
+                Context envContext = (Context) initialContext.lookup("java:comp/env");
+                dataSource = (DataSource) envContext.lookup("jdbc/servlet_app_db");
+
+            } catch (NamingException e) {
+                e.printStackTrace();
+            }
+        }
+        return dataSource;
+    }
+}
